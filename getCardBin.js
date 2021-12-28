34ce1e35a292c9564c4e581aed0fded918e46062
ryanlid <ryanlid@live.com>
2021-12-28 09:51:17 +0800

    修改卡号问题，错误提示

--- getCardBin.js
+++ getCardBin.js
@@ -7,20 +7,20 @@ const bankCardList = require('./bankCardList.js');
 module.exports = function getCardBin(cardNo) {
   return new Promise((resolve, reject) => {
     if (cardNo.toString().length < 15 || cardNo.toString().length > 19) {
-      reject(cardNo + ':银行卡位数必须是15到19位');
+      reject('银行卡位数必须是15到19位');
     }
 
     if (!cardNo || Number(cardNo) + '' === NaN + '') {
       reject('银行卡号必须是数字');
     }
 
-    for (var i = 0, len = bankCardList.length; i < len; i++) {
-      var bankcard = bankCardList[i];
-      var patterns = bankcard.patterns;
-      for (var j = 0, jLen = patterns.length; j < jLen; j++) {
-        var pattern = patterns[j];
+    for (let i = 0, len = bankCardList.length; i < len; i++) {
+      const bankcard = bankCardList[i];
+      const patterns = bankcard.patterns;
+      for (let j = 0, jLen = patterns.length; j < jLen; j++) {
+        const pattern = patterns[j];
         if (new RegExp(pattern.reg).test(cardNo)) {
-          var info = Object.assign({}, cardNo, bankcard, pattern);
+          const info = Object.assign({}, { cardNo: cardNo }, bankcard, pattern);
           delete info.patterns;
           delete info.reg;
           resolve(info);
